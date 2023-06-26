8635b608a9a5d647c154985c7bf9336233c1a55a
Jino Bae <im@baejino.com>
2023-06-27 00:38:50 +0900

    🔧 Fix test error

--- server/src/src/views/home/home.test.ts
+++ server/src/src/views/home/home.test.ts
@@ -4,7 +4,7 @@ import app from '~/app';
 
 describe('GET /home', () => {
     it('return text Hello, My Express JS!', async () => {
-        const res = await request(app).get('/home');
-        expect(res.text).toContain('Hello, My Express JS!'); 
+        const res = await request(app).get('/api/home');
+        expect(res.text).toContain('Hello, My Express JS!');
     });
 });
