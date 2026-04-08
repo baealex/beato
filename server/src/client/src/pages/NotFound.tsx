7fe6a21a71d159b2fd1901c3fc47ad67f946553d
Jino Bae <im@baejino.com>
2026-04-08 21:00:59 +0900

    🧹 Remove legacy UI client dependencies (#19)

--- server/src/client/src/pages/NotFound.tsx
+++ server/src/client/src/pages/NotFound.tsx
@@ -1,60 +1,21 @@
-import styled from '@emotion/styled';
 import { Button } from '~/components/shared';
 import { useBack } from '~/hooks';
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    gap: 1rem;
-`;
-
-const Dance = styled.div`
-    animation: dance 1.25s infinite ease-in;
-    font-size: 2rem;
-    font-weight: bold;
-
-    @keyframes dance {
-        0% {
-            transform: rotate(0deg);
-        }
-        25% {
-            transform: rotate(12deg);
-        }
-        50% {
-            transform: rotate(0deg);
-        }
-        75% {
-            transform: rotate(-12deg);
-        }
-        100% {
-            transform: rotate(0deg);
-        }
-    }
-`;
-
-const Text = styled.div`
-    opacity: 0.6;
-    font-size: 1.25rem;
-    margin-top: 1rem;
-`;
+import styles from './NotFound.module.scss';
 
 export default function NotFound() {
     const back = useBack();
 
     return (
-        <Container>
-            <Dance>
+        <div className={styles.container}>
+            <div className={styles.dance}>
                 {'(㇏(•̀ᵥᵥ•́)ノ)'}
-            </Dance>
-            <Text>
+            </div>
+            <div className={styles.text}>
                 Why are you here, huh?
-            </Text>
+            </div>
             <Button onClick={back}>
                 Take you back
             </Button>
-        </Container>
+        </div>
     );
 }
